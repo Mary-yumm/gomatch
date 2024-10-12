fc868e6787a3ef040dec73e0366f715ddcc67d48
Maryam Masood <maryammasood.fast@gmail.com>
2024-10-13 01:20:08 +0500

    adds settings screens

---
+++ lib/components/settings_screen/logout_confirmation_bottom_sheet.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:gomatch/utils/colors.dart';
+
+class LogoutConfirmationBottomSheet {
+  static void show(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+      ),
+      backgroundColor: AppColors.primaryColor,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Logout Confirmation', style: TextStyle(fontSize: 18, color: Colors.white)),
+              const SizedBox(height: 20),
+              const Text('Are you sure you want to logout?', style: TextStyle(color: Colors.white)),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context); // Close the bottom sheet
+                    },
+                    child: const Text('Cancel', style: TextStyle(color: Colors.white)),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Handle logout
+                      Navigator.pop(context); // Close the bottom sheet
+                    },
+                    child: const Text('Logout', style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
