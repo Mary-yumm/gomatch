300ad63071eaf8a86dfc5feff233fa526c929848
Maryam Masood <maryammasood.fast@gmail.com>
2024-10-13 13:45:12 +0500

    update histroy screen

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gomatch/components/history_screen/ride_history_list.dart';
 import 'package:gomatch/utils/colors.dart';
 
 class HistoryScreen extends StatelessWidget {
@@ -10,23 +11,12 @@ class HistoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Request History"),
+        title: const Text("My rides"),
         backgroundColor: AppColors.primaryColor,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height:40),
-            Text(
-              "Welcome to the History Screen!",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
+      body: const RideHistoryList(),
     );
   }
 }
