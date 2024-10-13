c6541ec28ccad12567220a12edff3d997b4ea483
Maryam Masood <maryammasood.fast@gmail.com>
2024-10-13 12:21:16 +0500

    add light primary color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -8,6 +8,7 @@ class AppColors {
   static const Color white = Colors.white24; // White
   static const Color textColor = Color(0xFF000000); // Black
   static const Color yellow = Color.fromARGB(201, 224, 135, 2);
+  static const Color lightPrimary = Color(0xFFa2b9c6); //light blue
   
   //grey 0xFFEDF4F2
   //static const Color yellow = Color.fromARGB(202, 249, 170, 51);
